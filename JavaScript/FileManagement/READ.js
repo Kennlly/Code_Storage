4caff2ecf9b636260478c458c3001564714fab44
Kenny He <kenny.he@itsp-inc.com>
2022-11-22 12:22:06 -0500

    updatedFileManagement

--- JavaScript/FileManagement/READ.js
+++ JavaScript/FileManagement/READ.js
@@ -9,11 +9,15 @@ const isFileExist = async (filePath) => {
 	}
 };
 
-const readTXTFile = async (isRecovery, timeStamp) => {
-	let filePath = isRecovery ? `${ERROR_FILEPATH}${timeStamp}.txt` : `${DATAFEEDRECORD_FILEPATH}${timeStamp}.txt`;
+const readTXTFile = async (fileName) => {
+	const filePath = `${TXT_FILEPATH}${fileName}.txt`;
 	try {
 		const isTXTFileExist = await isFileExist(filePath);
-		if (!isTXTFileExist) return undefined;
+		if (!isTXTFileExist) {
+			generalLogger.error(`File ${filePath} is NOT exist!`);
+			await writeTXTFile(fileName, "");
+			return "";
+		}
 
 		const data = await fs.readFile(filePath, "utf-8");
 		return data;
@@ -23,12 +27,14 @@ const readTXTFile = async (isRecovery, timeStamp) => {
 	}
 };
 
-const readJSONFile = async (category, timestamp) => {
-	const filePath = `${DATASTORAGE_FILEPATH}${category}${path.sep}${timestamp}_DBResult.json`;
+const readJSONFile = async (fileName) => {
+	const filePath = `${JSON_FILEPATH}${fileName}.json`;
 	try {
 		const isJSONFileExist = await isFileExist(filePath);
 		if (!isJSONFileExist) {
-			throw new Error(`File ${category}-${timestamp} is NOT exist!`);
+			generalLogger.error(`File ${filePath} is NOT exist!`);
+			await writeJSONFile(fileName, {});
+			return {};
 		}
 
 		const data = await fs.readFile(filePath, "utf-8");
@@ -36,10 +42,47 @@ const readJSONFile = async (category, timestamp) => {
 			return JSON.parse(data);
 		} catch (err) {
 			generalLogger.error(`Converting ${fileName}.json ${err}`);
-			return false;
 		}
 	} catch (err) {
-		recoverLogger.error(`readJSONFile Func ${err}. Filepath = ${filePath}`);
+		generalLogger.error(`readJSONFile Func reading ${filePath} ${err}`);
+		return false;
+	}
+};
+
+const asyncReadingFile = async (category, fileName) => {
+	//handle writing txt or json file
+	let filePath = -1;
+	try {
+		if (category !== "json" && category !== "txt") throw new Error(`Only input "json" or "txt" as reading file format!`);
+
+		if (category === "json") {
+			filePath = `${JSON_FILEPATH}${fileName}.${category}`;
+			const isJSONFileExist = await isFileExist(filePath);
+			if (!isJSONFileExist) {
+				generalLogger.error(`File ${filePath} is NOT exist!`);
+				await asyncWritingFile("json", fileName, {});
+				return {};
+			}
+		} else {
+			filePath = `${TXT_FILEPATH}${fileName}.${category}`;
+			const isTXTFileExist = await isFileExist(filePath);
+			if (!isTXTFileExist) {
+				generalLogger.error(`File ${filePath} is NOT exist!`);
+				await asyncWritingFile("txt", fileName, "");
+				return "";
+			}
+		}
+
+		const data = await fs.readFile(filePath, "utf-8");
+		if (category === "txt") return data;
+
+		try {
+			return JSON.parse(data);
+		} catch (err) {
+			generalLogger.error(`Converting ${fileName}.${category} ${err}`);
+		}
+	} catch (err) {
+		generalLogger.error(`asyncReadingFile Func ${err}`);
 		return false;
 	}
 };
--- JavaScript/FileManagement/WRITE.js
+++ JavaScript/FileManagement/WRITE.js
@@ -9,3 +9,39 @@ const writeJSONFile = async (fileName, content) => {
 		return false;
 	}
 };
+
+const writeTXTFile = async (fileName, content) => {
+	const filePath = `${TXT_FILEPATH}${fileName}.txt`;
+	try {
+		await fs.writeFile(filePath, content);
+		generalLogger.info(`Writing ${fileName}.txt SUCCEED!`);
+		return true;
+	} catch (err) {
+		generalLogger.error(`Writing ${fileName}.txt ${err}. Content = ${content}`);
+		return false;
+	}
+};
+
+const asyncWritingFile = async (category, fileName, content) => {
+	//handle writing txt or json file
+	let filePath = -1;
+	let fileContent = -1;
+	try {
+		if (category !== "json" && category !== "txt") throw new Error(`Only input "json" or "txt" as writing file format!`);
+
+		if (category === "json") {
+			filePath = `${JSON_FILEPATH}${fileName}.${category}`;
+			fileContent = JSON.stringify(content, null, 2);
+		} else {
+			filePath = `${TXT_FILEPATH}${fileName}.${category}`;
+			fileContent = content;
+		}
+
+		await fs.writeFile(filePath, fileContent);
+		generalLogger.info(`Writing ${fileName}.${category} SUCCEED!`);
+		return true;
+	} catch (err) {
+		generalLogger.error(`asyncWritingFile Func ${err}`);
+		return false;
+	}
+};
