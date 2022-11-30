525d8a8769fc81615e9cb624555fc08fc87fb910
KHG930 <59631629+Kennlly@users.noreply.github.com>
2022-11-30 14:01:38 -0500

    Refresh

--- JavaScript/FileManagement/general.js
+++
@@ -1,4 +0,0 @@
-import * as path from "path";
-
-//Based on OS to define the folder separator
-path.sep;
--- JavaScript/FileManagement/remove.js
+++
@@ -1,9 +0,0 @@
-const asyncRemoveFile = async (filePath) => {
-	try {
-		await fs.unlink(filePath);
-		return true;
-	} catch (err) {
-		generalLogger.error(`asyncRemoveFile Func ${err}. Filepath = ${filePath}`);
-		return false;
-	}
-};
